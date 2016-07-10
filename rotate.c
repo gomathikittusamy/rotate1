6cd45a0c75aa53ce89b859a1e566decc9391b57a
gomathikittusamy <gomsanu4@gmail.com>
2016-07-10 12:40:02 +0530

    Create rotate.c

---
+++ rotate.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[100],key,i,j,n,temp;
+clrscr();
+printf("enter the size of array");
+scanf("%d",&n);
+printf("Enter the element on by one");
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+printf("enter the key");
+scanf("%d",&key);
+printf("after rotatation");
+for(i=key;i<n;i++)
+{
+printf("%d",a[i]);
+}
+for(i=0;i<key;i++)
+
+printf("%d",a[i]);
+getch();
+}
+
+
